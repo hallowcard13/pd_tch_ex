24b1a1fde88e5f780f7f63441bfbb580a3707a1f
Hamid Reside <hamid.reside@gmail.com>
2023-10-19 02:41:05 +0800

    added fourth view

---
+++ lib/views/fourth_task_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FourthTaskView extends StatelessWidget {
+  const FourthTaskView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+        appBar: AppBar(automaticallyImplyLeading: true),
+        body: Center(child: Text('Fourth Task')));
+  }
+}
