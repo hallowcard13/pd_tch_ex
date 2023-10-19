5d48674d5c1ac65a0788d9a1540e80d577f5d2a9
Hamid Reside <hamid.reside@gmail.com>
2023-10-19 15:06:55 +0800

    installed bloc package

--- lib/views/fourth_task/fourth_task_index.dart
+++ lib/views/fourth_task/fourth_task_index.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
-class FourthTasks {
-  String? name;
+class FourthTaskView extends StatelessWidget {
+  const FourthTaskView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+      ),
+      body: Center(child: Text('Fourthtasks')),
+    );
+  }
 }
